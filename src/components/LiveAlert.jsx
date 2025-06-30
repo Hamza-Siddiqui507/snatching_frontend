7d3a12f74f0d8412d5a35285c1038d9b3aca56ef
Hamza Siddiqui <hamzasiddiqui507@gmail.com>
2025-06-30 10:33:04 +0500

    changes

--- src/components/LiveAlert.jsx
+++ src/components/LiveAlert.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://snatching-frontend.vercel.app/');
 
 export default function LiveAlert() {
     const [alerts, setAlerts] = useState([]);
